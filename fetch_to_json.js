43e3ea99343ddfd5ef89ca1cd590d8195c9d4860
Md. Ahammad Ali <146563026+ahmedstore75@users.noreply.github.com>
2026-08-31 16:32:22 +0600

    Update fetch_to_json.js

--- fetch_to_json.js
+++ fetch_to_json.js
@@ -132,7 +132,7 @@ async function fetchJsonData(url) {
 async function main() {
   const url1 = 'https://raw.githubusercontent.com/sm-monirulislam/Toffee-Auto-Update/refs/heads/main/toffee_playlist.m3u';
   const url2 = 'https://raw.githubusercontent.com/sm-monirulislam/SM-IPTV/refs/heads/main/akash_go.m3u';
-  const url3 = 'https://sm-monirul.top/api/app/info/channel_data.json';
+  const url3 = 'https://raw.githubusercontent.com/sm-monirulislam/Upcoming-and-Live-Sports-Data/refs/heads/main/Sports_data.m3u';
 
   console.log("Fetching channels...");
 
