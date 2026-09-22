8fd28d25e93a904cb2e4425d6bba407cb0f4928d
Md. Ahammad Ali <146563026+ahmedstore75@users.noreply.github.com>
2026-09-23 01:18:10 +0600

    Update fetch_to_json.js

--- fetch_to_json.js
+++ fetch_to_json.js
@@ -235,7 +235,7 @@ function processAndSortLink2(channels, seenUrls) {
 
 // মূল প্রসেসিং
 async function main() {
-  const url1 = 'https://raw.githubusercontent.com/srhady/axsports/refs/heads/main/playlist.m3u';
+  const url1 = 'https://raw.githubusercontent.com/srhady/SonyLiv/refs/heads/main/sonyliv_playlist.m3u';
   const url2 = 'https://raw.githubusercontent.com/srhady/toffee-bd/refs/heads/main/toffee_playlist.m3u';
   const url3 = 'https://raw.githubusercontent.com/ahan443/FAST-IPTV/refs/heads/main/z.m3u';
 
