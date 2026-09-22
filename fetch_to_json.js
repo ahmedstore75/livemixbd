4448ad9e0feff030e07207b88ece15a7c2e42078
Md. Ahammad Ali <146563026+ahmedstore75@users.noreply.github.com>
2026-09-23 01:14:43 +0600

    Update URL in fetch_to_json.js

--- fetch_to_json.js
+++ fetch_to_json.js
@@ -235,7 +235,7 @@ function processAndSortLink2(channels, seenUrls) {
 
 // মূল প্রসেসিং
 async function main() {
-  const url1 = 'https://gist.githubusercontent.com/albatr0ssss/3cff7a26be49b1d352c15f615067e7cd/raw/tapmad_bd.m3u';
+  const url1 = 'https://raw.githubusercontent.com/srhady/axsports/refs/heads/main/playlist.m3u';
   const url2 = 'https://raw.githubusercontent.com/srhady/toffee-bd/refs/heads/main/toffee_playlist.m3u';
   const url3 = 'https://raw.githubusercontent.com/ahan443/FAST-IPTV/refs/heads/main/z.m3u';
 
